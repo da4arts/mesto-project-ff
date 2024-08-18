6f0fa2d82dbefb06420805ee9ed1a9ae4b270f22
Artem Shikin <da4arts@gmail.com>
2024-08-18 15:27:39 +0300

    filling table with cards. activated trash button function

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,32 @@
 // @todo: Темплейт карточки
 
+
 // @todo: DOM узлы
+const container = document.querySelector('.content');
+const cardContainer = container.querySelector('.places__list');
+const addButton = container.querySelector('.profile__add-button');
 
 // @todo: Функция создания карточки
-
+function createCard(cardName, cardUrl) {
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    cardElement.querySelector('.card__image').src = cardUrl;
+    cardElement.querySelector('.card__image').textContent = cardName;
+    
+    cardElement.querySelector('.card__delete-button').addEventListener('click', removeCard);
+    
+    cardContainer.append(cardElement);
+}
 // @todo: Функция удаления карточки
 
+function removeCard(event) {
+    const eventTarget = event.target;
+    eventTarget.parentElement.remove();
+};
+
 // @todo: Вывести карточки на страницу
+
+initialCards.forEach(function(item) {
+    createCard(item.name, item.link);
+});
+
