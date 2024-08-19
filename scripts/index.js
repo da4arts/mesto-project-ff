d21bcade849ba9a5214e1856e056f2ca66ffaa83
Artem Shikin <da4arts@gmail.com>
2024-08-19 09:39:00 +0300

    Added callback to defition of createCard function. Added "alt" attribute filling. Changed removedCard function work from "parentElement" method to "closest

--- scripts/index.js
+++ scripts/index.js
@@ -7,13 +7,14 @@ const cardContainer = container.querySelector('.places__list');
 const addButton = container.querySelector('.profile__add-button');
 
 // @todo: Функция создания карточки
-function createCard(cardName, cardUrl) {
+function createCard(card, callback) {
     const cardTemplate = document.querySelector('#card-template').content;
     const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
-    cardElement.querySelector('.card__image').src = cardUrl;
-    cardElement.querySelector('.card__image').textContent = cardName;
+    cardElement.querySelector('.card__image').src = card.link;
+    cardElement.querySelector('.card__image').alt = "пейзаж из " + card.name;
+    cardElement.querySelector('.card__image').textContent = card.name;
     
-    cardElement.querySelector('.card__delete-button').addEventListener('click', removeCard);
+    cardElement.querySelector('.card__delete-button').addEventListener('click', callback);
     
     cardContainer.append(cardElement);
 }
@@ -21,12 +22,12 @@ function createCard(cardName, cardUrl) {
 
 function removeCard(event) {
     const eventTarget = event.target;
-    eventTarget.parentElement.remove();
+    eventTarget.closest('.card').remove();
 };
 
 // @todo: Вывести карточки на страницу
 
 initialCards.forEach(function(item) {
-    createCard(item.name, item.link);
+    createCard(item, removeCard);
 });
 
