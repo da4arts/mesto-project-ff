526e7131695c114ecc1186074c389f32c672adf5
Artem Shikin <da4arts@gmail.com>
2024-08-19 21:45:53 +0300

    corrected card title assignment

--- scripts/index.js
+++ scripts/index.js
@@ -7,12 +7,15 @@ const cardContainer = container.querySelector('.places__list');
 const addButton = container.querySelector('.profile__add-button');
 
 // @todo: Функция создания карточки
+
+//“It ain’t what you don’t know that gets you into trouble. It's what you know for sure that just ain't so." Mark Tawin
+
 function createCard(card, callback) {
     const cardTemplate = document.querySelector('#card-template').content;
     const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
     cardElement.querySelector('.card__image').src = card.link;
     cardElement.querySelector('.card__image').alt = "пейзаж из " + card.name;
-    cardElement.querySelector('.card__image').textContent = card.name;
+    cardElement.querySelector('.card__title').textContent = card.name;
     
     cardElement.querySelector('.card__delete-button').addEventListener('click', callback);
     
